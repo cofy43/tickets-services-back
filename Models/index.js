f48704350428897cc41deed63b8fbf6b9dd6578c
Martin Felipe Espinal Cruces <cofy43@gmail.com>
2022-07-31 13:47:32 -0500

    change file names

---
+++ Models/index.js
@@ -0,0 +1,24 @@
+const { associateModels } = require('./associations')
+const { Sequelize } = require('sequelize')
+const { database } = require('../config')
+
+const sequelize = new Sequelize(database.URL, database.OPTIONS)
+
+sequelize.sync(database.SYNC).then(() => {
+  // Sincroniza la base de datos
+  console.log('Sync db successful.')
+})
+
+const db = {
+  sequelize: sequelize,
+  Sequelize: Sequelize
+}
+
+db.client = require('./client.model')(sequelize);
+db.member = require('./member.model')(sequelize);
+db.status = require('./status.model')(sequelize);
+db.ticket = require('./ticket.model')(sequelize);
+
+associateModels(db)
+
+module.exports = db
