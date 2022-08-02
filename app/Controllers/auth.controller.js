c9bde2eb9375b608fa2e60d36e35264b7ebd0742
Martin Felipe Espinal Cruces <cofy43@gmail.com>
2022-08-02 17:06:19 -0500

    restore original domain

--- app/Controllers/auth.controller.js
+++ app/Controllers/auth.controller.js
@@ -36,7 +36,7 @@ module.exports = {
                 ),
                 {
                   secure: true,
-                  domain: ".netlify.app",
+                  domain: ".netlify",
                   sameSite: "none",
                   expires: moment().add(1, "d").toDate(),                  
                 }
@@ -85,7 +85,7 @@ module.exports = {
       ),
       {
         secure: true,
-        domain: ".netlify.app",
+        domain: ".netlify",
         expires: moment().toDate()
       }
     )
