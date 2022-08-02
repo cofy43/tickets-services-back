15d202461bafe38645fe13a25c008a65736b8a85
Martin Felipe Espinal Cruces <cofy43@gmail.com>
2022-08-02 17:09:47 -0500

    insecure cors condition

--- app/Controllers/auth.controller.js
+++ app/Controllers/auth.controller.js
@@ -36,7 +36,7 @@ module.exports = {
                 ),
                 {
                   secure: true,
-                  domain: ".netlify",
+                  domain: ".netlify.app",
                   sameSite: "none",
                   expires: moment().add(1, "d").toDate(),                  
                 }
@@ -85,7 +85,7 @@ module.exports = {
       ),
       {
         secure: true,
-        domain: ".netlify",
+        domain: ".netlify.app",
         expires: moment().toDate()
       }
     )
