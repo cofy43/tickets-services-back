55a9782f68d7d59234250401c0cef81641803a32
Martin Felipe Espinal Cruces <cofy43@gmail.com>
2022-08-02 17:14:16 -0500

    delte condition to app

--- index.js
+++ index.js
@@ -19,14 +19,6 @@ app.use(
   })
 );
 
-app.use((req, res, next) => {
-	res.header('Access-Control-Allow-Origin', '*');
-	res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, 	X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-	Method');
-	res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, 	DELETE');
-	res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
-	next();
-});
-
 app.use(requestIp.mw())
 
 routes(app) // Agregamos todas las rutas que se encuentran en routes/index.js
