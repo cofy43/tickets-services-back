85caca2d2306362761223fc0ec3779524e2812aa
Martin Felipe Espinal Cruces <cofy43@gmail.com>
2022-08-03 19:34:24 -0500

    add console to debug

--- app/Controllers/auth.controller.js
+++ app/Controllers/auth.controller.js
@@ -7,6 +7,7 @@ const { TOKEN_SECRET } = require('../Config')
 module.exports = {
   login(req, res) {
     const { email, password } = req.body;
+    console.log(req);
     db.member
       .findOne({
         where: {
